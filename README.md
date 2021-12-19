2db4cac6844ada18ccfcc7558f733d2b754e11cc
sanket <code.sanket13@gmail.com>
2021-12-20 03:27:12 +0530

    fourthCommit-20/12/2021

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## You need to install PyQt5 module for this. Copy the following command and paste it inside your terminal to install the required package
+## You need to install both PyQtWebEngine & PyQt5 modules for this. Copy the following commands and paste it inside your terminal to install the required packages.
+
+### pip install PyQtWebEngine
 
 ### pip install PyQt5
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
@@ -9,7 +10,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://google.com'))
+        self.browser.setUrl(QUrl('https://www.google.com/'))
         self.setCentralWidget(self.browser);
         self.showMaximized();
 
